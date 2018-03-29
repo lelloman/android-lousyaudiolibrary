72455dc4f21678e2d69e993c6efd0056e15a173c
lelloman <cerasuolodomenico@gmail.com>
2018-03-29 21:49:51 +0200

    libraries update

--- app/src/main/java/com/lelloman/lousyaudiolibrary/demo/MainActivity.java
+++ app/src/main/java/com/lelloman/lousyaudiolibrary/demo/MainActivity.java
@@ -11,28 +11,29 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		int size = 12;
-		double[] array = new double[size];
-		Arrays.fill(array, 10);
-		Log.d(MainActivity.class.getSimpleName(), Arrays.toString(array));
+        int size = 12;
+        double[] array = new double[size];
+        Arrays.fill(array, 10);
+        Log.d(MainActivity.class.getSimpleName(), Arrays.toString(array));
 
-		Fft fft = new Fft(size);
-		fft.realForward(array);
-		Log.d(MainActivity.class.getSimpleName(), Arrays.toString(array));
+        Fft fft = new Fft(size);
+        fft.realForward(array);
+        Log.d(MainActivity.class.getSimpleName(), Arrays.toString(array));
 
-		FragmentManager fragmentManager = getSupportFragmentManager();
-		if(fragmentManager.findFragmentById(R.id.container) == null){
-			fragmentManager.beginTransaction()
-					.add(R.id.container, PlayerFragment.newInstance(R.raw.ticotico), PlayerFragment.class.getSimpleName())
-					.commit();
-		}
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.findFragmentById(R.id.container) == null) {
+            fragmentManager.beginTransaction()
+                    .add(R.id.container, PlayerFragment.newInstance(R.raw.green_and_golden),
+                            PlayerFragment.class.getSimpleName())
+                    .commit();
+        }
 
-	}
+    }
 
 
 }
